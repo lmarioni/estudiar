231b5f7a411d274c8e22f90666e26835d24b712c
Matias Rivas <matias.rivas@appdirect.com>
2020-03-29 12:32:06 -0300

    Acomodado links y puesto tabla en Panel para poder ver el listado de alumnos

--- src/components/ListOfCourses/index.js
+++ src/components/ListOfCourses/index.js
@@ -5,57 +5,47 @@ import Button from 'react-bootstrap/Button';
 import QRCode from 'qrcode.react';
 
 export const ListOfCourses = ({ courses = [] }) => {
-    const [show, setShow] = useState(false);
-    const [inviteQR, setInviteQR] = useState('');
+  const [show, setShow] = useState(false);
+  const [inviteQR, setInviteQR] = useState('');
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const invite = (courseId) => {
-        console.log('Queres invitar al curso: ', courseId);
-        setInviteQR('inviteCode');
-        handleShow();
-    }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    return (
-        <React.Fragment>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Invitar</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h1>Usa este código QR para invitar a tus alumnos!</h1>
-                    <div>
-                        <QRCode
-                            id="123456"
-                            value={inviteQR}
-                            size={290}
-                            level={"H"}
-                            includeMargin={true}
-                        />
-                        
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Cerrar
+  const invite = (courseId) => {
+    setInviteQR(`https://www.estudi.ar/invite/${courseId}`);
+    handleShow();
+  }
+
+  return (
+    <React.Fragment>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Código de invitación</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="text-center"> <QRCode id="123456" value={inviteQR} size={290} level={"H"} includeMargin={true} /> </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
           </Button>
-                </Modal.Footer>
-            </Modal>
-            {
-                courses.map((course, i) => (
-                    <React.Fragment key={i}>
-                        <div className="card mb-4">
-                            <div className="card-body">
-                                <h4 className='mb-0'>{course.nombre}
-                                    <Link to={`/panel/${course.idcurso}`} className="btn btn-outline-primary float-right ml-1" > Alumnos </Link>
-                                    <Link to={`/course/${course.idcurso}`} className="btn btn-outline-primary float-right" > Ingresar al curso </Link>
-                                    <button onClick={() => invite(course.idcurso)} className="btn btn-outline-primary float-right mr-1"> Invitar </button>
-                                </h4>
-                            </div>
-                        </div>
-                    </React.Fragment>
-                ))
-            }
-        </React.Fragment>
-    )
+        </Modal.Footer>
+      </Modal>
+      {
+        courses.map((course, i) => (
+          <React.Fragment key={i}>
+            <div className="card mb-4">
+              <div className="card-body">
+                <h4 className='mb-0'>{course.nombre}
+                  <Link to={`/panel/${course.idcurso}`} className="btn btn-outline-primary float-right ml-1" > Alumnos </Link>
+                  <Link to={`/course/${course.idcurso}`} className="btn btn-outline-primary float-right" > Ingresar al curso </Link>
+                  <button onClick={() => invite(course.idcurso)} className="btn btn-outline-primary float-right mr-1"> Invitar </button>
+                </h4>
+              </div>
+            </div>
+          </React.Fragment>
+        ))
+      }
+    </React.Fragment>
+  )
 }
