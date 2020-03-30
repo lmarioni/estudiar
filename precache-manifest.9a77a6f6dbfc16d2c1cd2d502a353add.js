9fed332efbd3dde5a9a3f827f120f1be2ac454cf
Lucas Marioni <lucasmarionilm@gmail.com>
2020-03-29 21:04:31 -0300

    Updates

---
+++ precache-manifest.9a77a6f6dbfc16d2c1cd2d502a353add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a8e80cb64d5118b5ad582e8e0c5da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5621239d4d69d1907f09",
+    "url": "/static/css/main.cee250d9.chunk.css"
+  },
+  {
+    "revision": "9d5689ef9385edb76375",
+    "url": "/static/js/2.21e76a2c.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/static/js/2.21e76a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5621239d4d69d1907f09",
+    "url": "/static/js/main.9fafd6cb.chunk.js"
+  },
+  {
+    "revision": "32356e020c8b55ee6ff3",
+    "url": "/static/js/runtime-main.6343200c.js"
+  }
+]);
