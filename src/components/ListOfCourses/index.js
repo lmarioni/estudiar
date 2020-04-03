fbfbbc4939ad78d0d12a70d3276e1132780c2a32
Matias Rivas <matias.rivas@appdirect.com>
2020-04-03 19:57:08 -0300

    implementado DELETE de alumnos, ya podes echar gente que te cae mal

--- src/components/ListOfCourses/index.js
+++ src/components/ListOfCourses/index.js
@@ -1,82 +1,12 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import { Link } from '@reach/router';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import QRCode from 'qrcode.react';
 import './styles.scss';
-
-import { Letra, Icono, Title, Img, TeacherName } from './styles'
-
-import { MdPersonAdd, MdPersonOutline, MdBuild } from "react-icons/md";
+import { Letra, Icono, Title, Img, TeacherName } from './styles';
+import { MdBuild } from "react-icons/md";
 
 export const ListOfCourses = ({ courses = [] }) => {
-    const [show, setShow] = useState(false);
-    const [inviteQR, setInviteQR] = useState('');
-    const [copySuccess, setCopySuccess] = useState(false);
-    const [inviteCode, setInviteCode] = useState('');
-    const textAreaRef = useRef(null);
-
-
-    const copyToClipboard = () => {
-        const str = `https://estudiar.btcj.com.ar/i/${inviteCode}`;
-        const el = document.createElement('textarea');
-        el.value = str;
-        el.setAttribute('readonly', '');
-        el.style.position = 'absolute';
-        el.style.left = '-9999px';
-        document.body.appendChild(el);
-        const selected =
-            document.getSelection().rangeCount > 0
-                ? document.getSelection().getRangeAt(0)
-                : false;
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-        if (selected) {
-            document.getSelection().removeAllRanges();
-            document.getSelection().addRange(selected);
-        }
-        setCopySuccess(true);
-
-        setTimeout(()=>{
-            setCopySuccess(false);
-        }, 1500);
-
-    };
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const invite = (courseId, codigoInvitacion) => {
-        setInviteQR(`https://estudiar.btcj.com.ar/i/${inviteCode}`);
-        setInviteCode(codigoInvitacion);
-        handleShow();
-    }
-
     return (
         <React.Fragment>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Invitar</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h5 className="text-center">Usa este código QR para invitar a tus alumnos!</h5>
-                    <div className="text-center">
-                        <QRCode
-                            id="qrcode"
-                            value={inviteQR}
-                            size={290}
-                            level={"H"}
-                            includeMargin={true}
-                        />
-                    </div>
-                    <h5 className="text-center">Invita con esta url:</h5>
-                    <p className="link-url" ref={textAreaRef} onClick={copyToClipboard}>https://estudiar.btcj.com.ar/i/{inviteCode}</p>
-                    <p className={` text-center faded-text${copySuccess ? "-visible" : ""}`}>Copiado al portapapeles!</p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}> Cerrar </Button>
-                </Modal.Footer>
-            </Modal>
             {
                 courses.map((course, i) => {
                     var nombreCurso = course.nombre
@@ -95,8 +25,7 @@ export const ListOfCourses = ({ courses = [] }) => {
                                         <Title className='mb-0'>{course.nombre}
                                             {
                                                 course.creador && <React.Fragment>
-                                                    <Link to={`/panel/${course.idcurso}`} className="btn btn-outline-primary float-right mr-1" > <MdPersonOutline /> Alumnos </Link>
-                                                    <button onClick={() => invite(course.idcurso, course.codigoInvitacion)} className="btn btn-outline-primary float-right mr-1"> <MdPersonAdd /> Invitar </button>
+                                                    <Link to={`/panel/${course.idcurso}`} className="btn btn-outline-primary float-right mr-1" > <MdBuild /> Opciones </Link>
                                                 </React.Fragment>
                                             }
                                             <Link to={`/course/${course.idcurso}`} className="btn btn-outline-primary float-right mr-1" > Ingresar al curso </Link>
--- src/pages/Panel.js
+++ src/pages/Panel.js
@@ -1,14 +1,48 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { Context } from "../Context";
 import { Loading } from "../components/Loading";
 import Tab from "react-bootstrap/Tab";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
+import QRCode from 'qrcode.react';
 import { StudentsList } from "./StudentsList";
 export const Panel = ({ id }) => {
+  
   const { token } = useContext(Context);
   const [loading, setLoading] = useState(false);
+  
+  const [copySuccess, setCopySuccess] = useState(false);
+  const textAreaRef = useRef(null);
+  const [inviteCode, setInviteCode] = useState(id);
+  const [inviteQR, setInviteQR] = useState(`https://estudiar.btcj.com.ar/i/${inviteCode}`);
+
+  const copyToClipboard = () => {
+      const str = `https://estudiar.btcj.com.ar/i/${inviteCode}`;
+      const el = document.createElement('textarea');
+      el.value = str;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      const selected =
+          document.getSelection().rangeCount > 0
+              ? document.getSelection().getRangeAt(0)
+              : false;
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      if (selected) {
+          document.getSelection().removeAllRanges();
+          document.getSelection().addRange(selected);
+      }
+      setCopySuccess(true);
+
+      setTimeout(()=>{
+          setCopySuccess(false);
+      }, 1500);
+
+  };
 
   return (
     <div>
@@ -22,7 +56,7 @@ export const Panel = ({ id }) => {
                   <h1 className="text-center">Opciones</h1>
                 </div>
                 <div className="col-sm-12">
-                  <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                  <Tab.Container id="left-tabs-example" defaultActiveKey="studentList">
                     <Row>
                       <Col sm={3}>
                         <Nav variant="pills" className="flex-column">
@@ -30,18 +64,30 @@ export const Panel = ({ id }) => {
                             <Nav.Link eventKey="studentList">Listado de alumnos</Nav.Link>
                           </Nav.Item>
                           <Nav.Item>
-                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                            <Nav.Link eventKey="invite">Invitar</Nav.Link>
                           </Nav.Item>
                         </Nav>
                       </Col>
                       <Col sm={9}>
                         <Tab.Content>
                           <Tab.Pane eventKey="studentList">
-                           <StudentsList id={id}/>
+                            <StudentsList id={id} />
                           </Tab.Pane>
-                          <Tab.Pane eventKey="second">
-                           <h2>Second</h2>
-                            
+                          <Tab.Pane eventKey="invite">
+                            <h2>Invitar alumnos</h2>
+                            <h5 className="text-center">Usa este código QR para invitar a tus alumnos!</h5>
+                            <div className="text-center">
+                              <QRCode
+                                id="qrcode"
+                                value={inviteQR}
+                                size={290}
+                                level={"H"}
+                                includeMargin={true}
+                              />
+                            </div>
+                            <h5 className="text-center">Invita con esta url:</h5>
+                            <p className="link-url" ref={textAreaRef} onClick={copyToClipboard}>https://estudiar.btcj.com.ar/i/{inviteCode}</p>
+                            <p className={` text-center faded-text${copySuccess ? "-visible" : ""}`}>Copiado al portapapeles!</p>
                           </Tab.Pane>
                         </Tab.Content>
                       </Col>
