b49834d2742a293b2c41a84ded0019013e8860ee
Matias Rivas <matias.rivas@appdirect.com>
2020-04-06 21:59:05 -0300

    Gracias a bootstrap, tuve que cambiar los estilospara que no se superpongan, wek

--- src/components/Toasts/Toast.js
+++ src/components/Toasts/Toast.js
@@ -6,11 +6,11 @@ class Toast extends Component {
 
   render() {
     return (
-      <li className="toast" style={{ backgroundColor: this.props.color }}>
-        <p className="toast__content">
+      <li className="custom-toast" style={{ backgroundColor: this.props.color }}>
+        <p className="custom-toast__content">
           {this.props.text}
         </p>
-        <button className="toast__dismiss" onClick={this.props.onDismissClick}>
+        <button className="custom-toast__dismiss" onClick={this.props.onDismissClick}>
           x
         </button>
       </li>
