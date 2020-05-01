c07b9e80cbffe1bf9c215b14e63db6702b6dc252
Matias Rivas <matias.rivas@appdirect.com>
2020-05-01 00:46:06 -0300

    Mostrado cada uno del modulo en un accordeon card de reactjs

--- src/components/CourseLessons/index.js
+++ src/components/CourseLessons/index.js
@@ -4,6 +4,8 @@ import { Context } from '../../Context';
 import { Skeleton } from '../Skeleton';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
 import { MdBuild } from "react-icons/md";
 import { ListCard } from "../ListCard";
 
@@ -36,23 +38,43 @@ const CourseLessons = (course) => {
 
     const openModal = (lessonId) => {
         const lesson = lessons.find(lesson => lesson.id === lessonId);
-        console.log({lesson});
+        console.log({ lesson });
         setLesson(lesson);
         setShow(true);
     }
 
-    const handleClose = () => { 
+    const handleClose = () => {
         setLesson({});
         setShow(false);
-     }
+    }
 
     return (
         <div>
-            <Modal show={show} onHide={handleClose}>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                size="lg"
+                aria-labelledby="lesson-edit-modal">
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>{lesson.nombre}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+
+                    <Accordion defaultActiveKey="0">
+                        {lesson.modulos && lesson.modulos.map(lesson => {
+                            return (
+                                <Card key={`module-${lesson.id}`}>
+                                    <Accordion.Toggle as={Card.Header} eventKey={lesson.id}>  {lesson.nombre} </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey={lesson.id}>
+                                        <Card.Body>{lesson.contenido}</Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            )
+                        }
+                        )}
+
+                    </Accordion>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}> Close </Button>
                     <Button variant="primary" onClick={handleClose}> Save Changes </Button>
@@ -70,7 +92,7 @@ const CourseLessons = (course) => {
                                         <ListCard
                                             title={courseLesson.nombre}
                                             subtitle={`${courseLesson.modulos.length} modulos`}
-                                            action={<Button onClick={() => {openModal(courseLesson.id)}} className="float-right"> <MdBuild />  </Button>}
+                                            action={<Button onClick={() => { openModal(courseLesson.id) }} className="float-right"> <MdBuild />  </Button>}
                                         />
                                     </React.Fragment>
                                 )
