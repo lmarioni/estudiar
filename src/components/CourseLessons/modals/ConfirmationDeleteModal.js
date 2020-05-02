b386b1af1705a2e6d27891a430a72afea9815ddf
Matias Rivas <matias.rivas@appdirect.com>
2020-05-02 01:56:12 -0300

    Separado en modales, e implementado modal de delete

---
+++ src/components/CourseLessons/modals/ConfirmationDeleteModal.js
@@ -0,0 +1,67 @@
+
+import React, { useEffect, useState, useContext } from 'react';
+import { Context } from '../../../Context';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+const ConfirmationDeleteModal = ({ showModal, lessonToDelete, callback }) => {
+    const { token } = useContext(Context);
+    const [show, setShow] = useState(true);
+    const [lesson, setLesson] = useState(true);
+    const [disableButton, setDisableButton] = useState(false);
+
+    useEffect(function () {
+        setShow(showModal);
+        setLesson(lessonToDelete);
+    }, [{lessonToDelete}]);
+
+    const handleCloseConfirmationModal = () => {
+        setLesson({});
+        setShow(false);
+        callback({close: true, delete: false, status: 'success', message: ''});
+    }
+
+    const confirmDeleteLesson = () => {
+        async function removeLesson() {
+            setDisableButton(true);
+            const requestOptions = {
+                method: 'DELETE',
+                headers: new Headers({
+                    authorization: `Bearer ${token}`, 'Accept': 'application/json', 'Content-Type': 'application/json'
+                }),
+            };
+            const response = await fetch(`${process.env.REACT_APP_BASE_URL}/lecciones/${lesson.id}`, requestOptions);
+            const parsedResponse = await response.json();
+            if (parsedResponse.status === 'success') {
+                callback({close: true, delete: true, status: 'success', message: parsedResponse.message});
+            } else {
+                callback({close: true, delete: true, status: 'error', message: 'Hubo un error, intentelo nuevamente.'});
+            }
+        }
+        removeLesson();
+        setDisableButton(false);
+    }
+
+    return (
+        <div>
+            {
+                show ? (
+                    <Modal show={show} onHide={handleCloseConfirmationModal} size="lg" aria-labelledby="delete-lesson-confirmation-modal" centered >
+                        <Modal.Header closeButton>
+                            <Modal.Title>¿Está seguro que desea eliminar la lección?</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <div> Está a punto de eliminar la lección <p className="font-weight-bold">"{lesson.nombre}"</p> </div>
+                            <div> Una vez eliminada, no podrá recuperarla. </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" onClick={handleCloseConfirmationModal}> No, cerrar </Button>
+                            <Button variant="secondary" disabled={disableButton} onClick={confirmDeleteLesson}> Si, eliminar </Button>
+                        </Modal.Footer>
+                    </Modal>
+                ) : null
+            }
+        </div>
+    )
+}
+export default ConfirmationDeleteModal;
