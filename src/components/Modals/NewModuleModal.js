03f69ddec3321865d24590840217f8e99d9ffae7
Matias Rivas <matias.rivas@appdirect.com>
2020-05-03 18:31:04 -0300

    Modificado el create new module e implementado el edit module

--- src/components/Modals/NewModuleModal.js
+++ src/components/Modals/NewModuleModal.js
@@ -16,10 +16,14 @@ const NewModuleModal = ({ fulllesson, showModal, callback }) => {
     const [show, setShow] = useState(false);
     const [lesson, setLesson] = useState({});
     const [disableButton, setDisableButton] = useState(false);
-    const [htmlEditorValue, setHtmlEditorValue] = useState('');
-    const [moduleVisible, setModuleVisible] = useState(false);
 
     const [moduleTitle, setModuleTitle] = useState('');
+    const [moduleDescription, setModuleDescription] = useState('');
+    const [contentType, setContentType] = useState(1);
+    const [content, setContent] = useState('');
+    const [moduleVisible, setModuleVisible] = useState(true);
+    const [urlVideo, setUrlVideo] = useState('');
+    const [htmlEditorValue, setHtmlEditorValue] = useState('');
 
     useEffect(function () {
         if (lesson !== fulllesson || showModal !== show) {
@@ -40,18 +44,26 @@ const NewModuleModal = ({ fulllesson, showModal, callback }) => {
         event.stopPropagation();
         async function submitModule() {
             setDisableButton(true);
+            const payload = {};
+            payload.nombre = moduleTitle;
+            payload.descripcion = moduleDescription;
+            payload.visible = moduleVisible;
+            payload.tipo = parseInt(contentType);
+            switch (parseInt(contentType)) {
+                case 1:
+                    payload.contenido = content;
+                    payload.urlVideo = urlVideo;
+                    break;
+                case 4:
+                    payload.contenido = htmlEditorValue;
+                    break;
+            }
             const requestOptions = {
                 method: 'POST',
                 headers: new Headers({
                     authorization: `Bearer ${token}`, 'Accept': 'application/json', 'Content-Type': 'application/json'
                 }),
-                body: JSON.stringify({
-                    nombre: moduleTitle,
-                    visible: moduleVisible,
-                    tipo: 1, 
-                    evalid: 0,
-                    contenido: htmlEditorValue
-                }),
+                body: JSON.stringify(payload),
             };
             const response = await fetch(`${process.env.REACT_APP_BASE_URL}/lecciones/${lesson.id}/modulos`, requestOptions);
             const parsedResponse = await response.json();
@@ -63,12 +75,8 @@ const NewModuleModal = ({ fulllesson, showModal, callback }) => {
         }
         submitModule();
         setDisableButton(false);
-        
 
-    }
 
-    const handleSwitch = (event) => {
-        setModuleVisible(event.target.checked);
     }
 
     return (
@@ -80,20 +88,44 @@ const NewModuleModal = ({ fulllesson, showModal, callback }) => {
                             <Modal.Title>{lesson.nombre} - Nuevo módulo</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <Form noValidate onSubmit={() => {handleSubmit()}}>
-                                <Form.Group controlId="formNewModule">
+                            <Form noValidate onSubmit={() => { handleSubmit() }}>
+                                <Form.Group controlId="moduleTitle">
                                     <Form.Control type="text" placeholder="Ingrese un título" value={moduleTitle} onChange={e => setModuleTitle(e.target.value)} />
                                 </Form.Group>
-
+                                <Form.Group controlId="moduleDescription">
+                                    <Form.Control type="text" placeholder="Ingrese una descripción para el modulo" value={moduleDescription} onChange={e => setModuleDescription(e.target.value)} />
+                                </Form.Group>
                                 <Form.Check
-                                    onChange={handleSwitch}
+                                    onChange={e => setModuleVisible(event.target.checked)}
                                     type="switch"
+                                    value={moduleVisible}
                                     id="custom-switch"
                                     label={moduleVisible ? 'Visible para los alumnos' : 'No visible para los alumnos'}
                                 />
-                                <CustomToolbar />
-                                <ReactQuill theme="snow" value={htmlEditorValue} onChange={setHtmlEditorValue} modules={modules}
-                                    formats={formats} />
+                                <Form.Group controlId="contentType">
+                                    <Form.Label>Elija un tipo de contenido</Form.Label>
+                                    <Form.Control as="select" value={contentType} onChange={e => setContentType(e.target.value)}>
+                                        <option value="1">Video con texto sin formato</option>
+                                        <option value="4">Texto con formato</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                {contentType == 1 ? (
+                                    <div>
+                                        <Form.Group controlId="simpleContent">
+                                            <Form.Label>Contenido módulo</Form.Label>
+                                            <Form.Control type="text" placeholder="Ingrese el contenido" value={content} onChange={e => setContent(e.target.value)} />
+                                        </Form.Group>
+                                        <Form.Group controlId="videoLink">
+                                            <Form.Label>URL del video</Form.Label>
+                                            <Form.Control type="text" placeholder="Ingrese la url del video" value={urlVideo} onChange={e => setUrlVideo(e.target.value)} />
+                                        </Form.Group>
+                                    </div>
+                                ) : null
+                                }
+
+                                {contentType == 4 ? (<div><CustomToolbar />
+                                    <ReactQuill theme="snow" value={htmlEditorValue} onChange={setHtmlEditorValue} modules={modules}
+                                        formats={formats} /></div>) : null}
                             </Form>
                         </Modal.Body>
                         <Modal.Footer>
