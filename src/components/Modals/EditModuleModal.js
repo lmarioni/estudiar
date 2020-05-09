7cf2f65653ad1059a15adfe38231c211a61f4b23
Matias Rivas <matias.rivas@appdirect.com>
2020-05-09 12:55:55 -0300

    Cambiado el manejo de archivos yta que no son mas un array son uno solo

--- src/components/Modals/EditModuleModal.js
+++ src/components/Modals/EditModuleModal.js
@@ -107,7 +107,7 @@ const EditModuleModal = ({ fullModule, showModal, callback }) => {
         setDisableButton(false);
     }
 
-    const handleUpdateFiles = (fileItems) => { setFiles(fileItems[0].file); }
+    const handleUpdateFiles = (fileItems) => { setFiles(fileItems.file); }
 
     return (
         <div>
