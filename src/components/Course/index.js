0eedce280c090ec53e56e749ee9a9cce7595c825
Matias Rivas <matias.rivas@appdirect.com>
2020-05-10 12:43:28 -0300

    Reidentado

--- src/components/Course/index.js
+++ src/components/Course/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import ReactPlayer from 'react-player'
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 import { Menu } from "./Menu";
 import { MenuMobile } from "./MenuMobile";
@@ -11,7 +11,7 @@ export const Course = ({ curso }) => {
   const [moduleSelected, setSelected] = useState({});
 
   useEffect(function () {
-    
+
   }, []);
 
 
@@ -28,30 +28,30 @@ export const Course = ({ curso }) => {
     <React.Fragment>
       <Helmet>
         <title> {curso.nombre ? curso.nombre : 'Cargando...'} | Estudi.ar </title>
-        </Helmet>
+      </Helmet>
       <div className="fluid-container">
         <div className="row">
           {curso.lecciones && (
             <React.Fragment>
-            <Menu
-              lecciones={curso.lecciones}
-              onModuleChange={(idLeccion, idModulo) => {
-                cambiarModulo(idLeccion, idModulo);
-              }}
-            />
-            
+              <Menu
+                lecciones={curso.lecciones}
+                onModuleChange={(idLeccion, idModulo) => {
+                  cambiarModulo(idLeccion, idModulo);
+                }}
+              />
+
               <MenuMobile
-              lecciones={curso.lecciones}
-              onModuleChange={(idLeccion, idModulo) => {
-                cambiarModulo(idLeccion, idModulo);
-              }}
+                lecciones={curso.lecciones}
+                onModuleChange={(idLeccion, idModulo) => {
+                  cambiarModulo(idLeccion, idModulo);
+                }}
               />
 
             </React.Fragment>
-           
+
           )}
 
-              <div className="col-md-1"></div>
+          <div className="col-md-1"></div>
           <div className="col-md-6">
             {moduleSelected.id ? (
               <React.Fragment>
@@ -62,53 +62,53 @@ export const Course = ({ curso }) => {
                   {" "}
                   {moduleSelected.nombre}
                 </h2>
-                
+
                 <div
                   className="contenido-modulo"
                   dangerouslySetInnerHTML={{ __html: moduleSelected.contenido }}
                 >
-                  
+
                 </div>
 
-              {
-                moduleSelected.tipo === 1 && <>
-                <div className="text-center">
-                  <div className='player-wrapper'>
-                    <ReactPlayer
-                      className='react-player'
-                      url={moduleSelected.urlVideo}
-                      width='100%'
-                      height='100%'
-                      controls
-                    />
-                  </div>
-                </div>
-                </>
-              }
+                {
+                  moduleSelected.tipo === 1 && <>
+                    <div className="text-center">
+                      <div className='player-wrapper'>
+                        <ReactPlayer
+                          className='react-player'
+                          url={moduleSelected.urlVideo}
+                          width='100%'
+                          height='100%'
+                          controls
+                        />
+                      </div>
+                    </div>
+                  </>
+                }
 
-              {
-                moduleSelected.tipo === 3 && <>
-                <p className="text-center"> Si no puedes ver el documento <a target="_blank" href={moduleSelected.urlDocumento}> haz click aqui </a> </p>
-                <iframe  width="100%" height="700" frameborder="1" src={`https://docs.google.com/gview?url=${moduleSelected.urlDocumento}&embedded=true`}></iframe>
-                </>
-              }
+                {
+                  moduleSelected.tipo === 3 && <>
+                    <p className="text-center"> Si no puedes ver el documento <a target="_blank" href={moduleSelected.urlDocumento}> haz click aqui </a> </p>
+                    <iframe width="100%" height="700" frameborder="1" src={`https://docs.google.com/gview?url=${moduleSelected.urlDocumento}&embedded=true`}></iframe>
+                  </>
+                }
 
               </React.Fragment>
             ) : <div className="text-center mt-5">
 
-              {/* <img src={curso.imagen} alt=""/> */}
-              <h2>Bienvenido!!</h2>
-              <p>Selecciona un modulo para comenzar</p>
-              
-              <div className="explicacion-mobile">
-                <p>Encuentra tus lecciones en tu esquina inferior derecha</p>
-              </div>
+                {/* <img src={curso.imagen} alt=""/> */}
+                <h2>Bienvenido!!</h2>
+                <p>Selecciona un modulo para comenzar</p>
 
-              <div className="explicacion-pc">
-                <p>Encuentra tus lecciones a tu izquierda</p>
-              </div>
+                <div className="explicacion-mobile">
+                  <p>Encuentra tus lecciones en tu esquina inferior derecha</p>
+                </div>
 
-            </div>
+                <div className="explicacion-pc">
+                  <p>Encuentra tus lecciones a tu izquierda</p>
+                </div>
+
+              </div>
             }
           </div>
         </div>
