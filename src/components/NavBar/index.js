6afec20d1b48b411d2a982c34a160a168f1f7547
Matias Rivas <matias.rivas@appdirect.com>
2020-05-24 11:03:45 -0300

    Cambiado el color del navbar para que resalte. y Agregado link a mis cursos

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import './styles.css';
+
 export const NavBar = () => {
 
     return (
-        <Navbar collapseOnSelect expand="lg" style={{ background: 'white !important', borderBottom: '1px solid #EBEBEB' }}>
+        <Navbar collapseOnSelect expand="lg" className="mainNavbar">
             <Navbar.Brand>
-            <Link to="/" className="navbar-brand" > Estudi.ar </Link>
+                <Link to="/" className="navbar-brand" > Estudi.ar </Link>
             </Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                <Link to="/logout">Salir</Link>
-                </Navbar.Text>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Mis cursos</Nav.Link>
+                </Nav>
+                <Nav className="justify-content-end">
+                    <Navbar.Text>
+                        <Link to="/logout">Salir</Link>
+                    </Navbar.Text>
+                </Nav>
+
             </Navbar.Collapse>
         </Navbar>
     )
--- src/components/NavBar/styles.js
+++
@@ -1,7 +0,0 @@
-
-    import styled from 'styled-components'
-
-export const Nav = styled.nav`
-    background-color: white !important;
-    border-bottom: 1px solid rgb(217, 215, 215)
-`
